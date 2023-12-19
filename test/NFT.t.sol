648dae813764ac87de60db48591073c48bbd16bf
James Farrell <james@toucan.earth>
2023-12-19 17:07:00 +0100

    feat: tests for NFT contract

--- test/NFT.t.sol
+++ test/NFT.t.sol
@@ -1,136 +1,64 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
-import "../src/NFT.sol";
+import "../src/NFT.sol"; // Update the import path as needed
 
 contract NFTTest is Test {
-    using stdStorage for StdStorage;
-
-    NFT private nft;
+    NFT nft;
+    address recipient = address(0x123);
 
     function setUp() public {
-        // Deploy NFT contract
-        nft = new NFT("NFT_tutorial", "TUT", "baseUri");
+        nft = new NFT("TestNFT", "TNFT", "https://example.com/");
     }
 
-    function test_RevertMintWithoutValue() public {
-        vm.expectRevert(MintPriceNotPaid.selector);
-        nft.mintTo(address(1));
-    }
-
-    function test_MintPricePaid() public {
-        nft.mintTo{value: 0.08 ether}(address(1));
-    }
-
-    function test_RevertMintMaxSupplyReached() public {
-        uint256 slot = stdstore
-            .target(address(nft))
-            .sig("currentTokenId()")
-            .find();
-        bytes32 loc = bytes32(slot);
-        bytes32 mockedCurrentTokenId = bytes32(abi.encode(10000));
-        vm.store(address(nft), loc, mockedCurrentTokenId);
-        vm.expectRevert(MaxSupply.selector);
-        nft.mintTo{value: 0.08 ether}(address(1));
-    }
-
-    function test_RevertMintToZeroAddress() public {
-        vm.expectRevert("INVALID_RECIPIENT");
-        nft.mintTo{value: 0.08 ether}(address(0));
-    }
-
-    function test_NewMintOwnerRegistered() public {
-        nft.mintTo{value: 0.08 ether}(address(1));
-        uint256 slotOfNewOwner = stdstore
-            .target(address(nft))
-            .sig(nft.ownerOf.selector)
-            .with_key(1)
-            .find();
-
-        uint160 ownerOfTokenIdOne = uint160(
-            uint256(
-                (vm.load(address(nft), bytes32(abi.encode(slotOfNewOwner))))
-            )
+    function testMintTo() public {
+        uint256 tokenId = nft.mintTo(recipient);
+        assertEq(
+            nft.ownerOf(tokenId),
+            recipient,
+            "Recipient should own the new token"
         );
-        assertEq(address(ownerOfTokenIdOne), address(1));
     }
 
-    function test_BalanceIncremented() public {
-        nft.mintTo{value: 0.08 ether}(address(1));
-        uint256 slotBalance = stdstore
-            .target(address(nft))
-            .sig(nft.balanceOf.selector)
-            .with_key(address(1))
-            .find();
-
-        uint256 balanceFirstMint = uint256(
-            vm.load(address(nft), bytes32(slotBalance))
+    function testTokenURI() public {
+        uint256 tokenId = nft.mintTo(recipient);
+        string memory expectedURI = string(
+            abi.encodePacked("https://example.com/", Strings.toString(tokenId))
         );
-        assertEq(balanceFirstMint, 1);
-
-        nft.mintTo{value: 0.08 ether}(address(1));
-        uint256 balanceSecondMint = uint256(
-            vm.load(address(nft), bytes32(slotBalance))
+        assertEq(
+            nft.tokenURI(tokenId),
+            expectedURI,
+            "tokenURI should return the correct string"
         );
-        assertEq(balanceSecondMint, 2);
     }
 
-    function test_SafeContractReceiver() public {
-        Receiver receiver = new Receiver();
-        nft.mintTo{value: 0.08 ether}(address(receiver));
-        uint256 slotBalance = stdstore
-            .target(address(nft))
-            .sig(nft.balanceOf.selector)
-            .with_key(address(receiver))
-            .find();
+    function testWithdrawPayments() public {
+        // Setup: Mint a token to ensure there's some balance in the contract
+        nft.mintTo(recipient);
 
-        uint256 balance = uint256(vm.load(address(nft), bytes32(slotBalance)));
-        assertEq(balance, 1);
-    }
+        // Simulate sending ether to the contract (if needed)
+        payable(address(nft)).transfer(1 ether);
 
-    function test_RevertUnSafeContractReceiver() public {
-        // Adress set to 11, because first 10 addresses are restricted for precompiles
-        vm.etch(address(11), bytes("mock code"));
-        vm.expectRevert(bytes(""));
-        nft.mintTo{value: 0.08 ether}(address(11));
-    }
+        // Withdraw balance
+        uint256 contractBalanceBefore = address(nft).balance;
+        address payable payee = payable(address(this));
+        uint256 payeeBalanceBefore = payee.balance;
 
-    function test_WithdrawalWorksAsOwner() public {
-        // Mint an NFT, sending eth to the contract
-        Receiver receiver = new Receiver();
-        address payable payee = payable(address(0x1337));
-        uint256 priorPayeeBalance = payee.balance;
-        nft.mintTo{value: nft.MINT_PRICE()}(address(receiver));
-        // Check that the balance of the contract is correct
-        assertEq(address(nft).balance, nft.MINT_PRICE());
-        uint256 nftBalance = address(nft).balance;
-        // Withdraw the balance and assert it was transferred
         nft.withdrawPayments(payee);
-        assertEq(payee.balance, priorPayeeBalance + nftBalance);
-    }
 
-    function test_WithdrawalFailsAsNotOwner() public {
-        // Mint an NFT, sending eth to the contract
-        Receiver receiver = new Receiver();
-        nft.mintTo{value: nft.MINT_PRICE()}(address(receiver));
-        // Check that the balance of the contract is correct
-        assertEq(address(nft).balance, nft.MINT_PRICE());
-        // Confirm that a non-owner cannot withdraw
-        vm.expectRevert("Ownable: caller is not the owner");
-        vm.startPrank(address(0xd3ad));
-        nft.withdrawPayments(payable(address(0xd3ad)));
-        vm.stopPrank();
-    }
-}
+        uint256 contractBalanceAfter = address(nft).balance;
+        uint256 payeeBalanceAfter = payee.balance;
 
-contract Receiver is ERC721TokenReceiver {
-    function onERC721Received(
-        address operator,
-        address from,
-        uint256 id,
-        bytes calldata data
-    ) external override returns (bytes4) {
-        return this.onERC721Received.selector;
+        assertEq(
+            contractBalanceAfter,
+            0,
+            "Contract balance should be zero after withdrawal"
+        );
+        assertEq(
+            payeeBalanceAfter,
+            payeeBalanceBefore + contractBalanceBefore,
+            "Payee balance should increase by the withdrawn amount"
+        );
     }
 }
